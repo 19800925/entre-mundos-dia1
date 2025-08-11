a0b9b8bda7493a0f138069508c153a7d41f9028d
19800925 <goncalosamtavares@gmail.com>
2025-08-11 11:28:59 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-const CACHE = 'entre-mundos-dia1-v1';
+const CACHE_NAME = 'entre-mundos-dia1-v1';
 const ASSETS = [
   './',
   './index.html',
+  './style.css',
   './manifest.json',
   './assets/icon-192.png',
-  './assets/icon-512.png'
+  './assets/IMG_2247.png'
 ];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-self.addEventListener('activate', e => {
+
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
 });
-self.addEventListener('fetch', e => {
+
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
